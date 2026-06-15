00450203d9699f3253f2c2d8381bf2f3efb70dae
youngmonero <yungmonero29@gmail.com>
2026-06-16 00:33:53 +0100

    work complete

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Paste your exact configuration object keys from your Firebase dashboard below
 const firebaseConfig = {
-  apiKey: "AIzaSyAK_IBgDgaMsT7cz3TMGBM65KMLCZ6HjDo",
-  authDomain: "buildconnect-recruitment.firebaseapp.com",
-  projectId: "buildconnect-recruitment",
-  storageBucket: "buildconnect-recruitment.firebasestorage.app",
-  messagingSenderId: "1016829882361",
-  appId: "1:1016829882361:web:b626bc46485fe834955728",
-  measurementId: "G-NNHG3LKDKY"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase services
@@ -19,4 +18,4 @@ const app = initializeApp(firebaseConfig);
 
 // Export instances to use them in our components
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app)
